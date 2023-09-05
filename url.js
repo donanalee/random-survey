0fe7b7cf9267fb470e57f0e4eca0f1e2b3800b7b
donanalee <144093416+donanalee@users.noreply.github.com>
2023-09-05 13:54:30 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://forms.gle/RAfC3zuB57LxFoja7',
+	'https://forms.gle/mCdHEZfEBMaTuwLt6'
 ];
